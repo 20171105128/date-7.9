d9e3cd5494b5e43bda8cab125f30dc2c98024535
20171105128 <s20171105128@admindeimac-88.local>
2018-07-09 11:52:04 +0800

    Initial Commit

---
+++ date 7.9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  date 7.9
+//
+//  Created by TAO on 18/7/9.
+//  Copyright © 2018年 TAO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
